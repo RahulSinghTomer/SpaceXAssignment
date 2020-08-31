94dd687dffdc74f31b9ca3f202e561d885a31ad5
RahulTomer <rahul.tomer@kiwitech.com>
2020-08-31 23:35:25 +0530

    assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SpaceX';
+  totalSpaceXLaunches:Array<any>=[];
+  constructor(private http: HttpClient) { }
+  ngOnInit() {      
+    // Simple GET request with response type <any>
+    this.http.get<any>('https://api.spacexdata.com/v3/launches?limit=100').subscribe(data => {
+      console.log(data);
+        this.totalSpaceXLaunches = data;
+        
+    })
+}
+filterlaunchSuccess(launchSuccess){
+  this.http.get<any>(`https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=${launchSuccess}`).subscribe(data => {
+    console.log(data);
+      this.totalSpaceXLaunches = data;
+      
+  })
+  
+}
 }
